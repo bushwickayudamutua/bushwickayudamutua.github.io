371ce2511a8941e74b5f7ae059fa316dcd940605
Brian <brian@abelson.live>
2024-12-31 13:52:23 -0500

    format

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    const setSpanish = function() {
+    const setSpanish = function () {
         localStorage.setItem('lang', 'span');
         $('.lang-btn.eng').removeClass('active');
         $('.span.lang-btn').addClass('active');
@@ -9,7 +9,7 @@ $(document).ready(function(){
         $('.lang-text.span').addClass('active');
     }
 
-    const setEnglish = function() {
+    const setEnglish = function () {
         localStorage.setItem('lang', 'eng');
         $('.span.lang-btn').removeClass('active');
         $('.lang-btn.eng').addClass('active');
@@ -48,7 +48,7 @@ $(document).ready(function(){
     const openRequestsURL = "https://file.baml.ink/website-data/open-requests.json";
     var openRequestsDiv = document.getElementById("requests-counter-container");
     if (openRequestsDiv != null) {
-        $.getJSON(openRequestsURL, function(data) {
+        $.getJSON(openRequestsURL, function (data) {
             var metrics = data.metrics;
             var nMetrics = metrics.length;
             var half = Math.ceil(nMetrics / 2);
@@ -56,7 +56,7 @@ $(document).ready(function(){
             var secondHalf = metrics.slice(half, nMetrics);
             var halves = [firstHalf, secondHalf];
             var html = '';
-            $.each(halves, function(key, half) {
+            $.each(halves, function (key, half) {
                 html += '<div class="request-column">';
                 for (var i = 0; i < half.length; i++) {
                     var request = half[i];
@@ -77,14 +77,46 @@ $(document).ready(function(){
         });
     };
 
+    // populate requests counter on homepage by fetching data from a JSON file on Digital Ocean Spaces.
+    const fulfilledRequestsURL = "https://file.baml.ink/website-data/fulfilled-requests.json";
+    var fulfilledRequestsDiv = document.getElementById("fulfilled-requests-counter-container");
+    if (fulfilledRequestsDiv != null) {
+        $.getJSON(fulfilledRequestsURL, function (data) {
+            var metrics = data.metrics;
+            var nMetrics = metrics.length;
+            var half = Math.ceil(nMetrics / 2);
+            var firstHalf = metrics.slice(0, half);
+            var secondHalf = metrics.slice(half, nMetrics);
+            var halves = [firstHalf, secondHalf];
+            var html = '';
+            $.each(halves, function (key, half) {
+                html += '<div class="request-column">';
+                for (var i = 0; i < half.length; i++) {
+                    var request = half[i];
+                    html += "<div class='request-entry'>";
+                    html += "<h2 style='display: inline;' class='request-count'>" + request.value + " </h2>";
+                    if (localStorage.getItem('lang') === 'span') {
+                        html += "<span class='span request-category lang-text active'> " + request.translations.span + "</span>";
+                        html += "<span class='eng request-category lang-text'>  " + request.translations.eng + "</span>";
+                    } else {
+                        html += "<span class='span request-category lang-text'> " + request.translations.span + "</span>";
+                        html += "<span class='eng request-category lang-text active'>  " + request.translations.eng + "</span>";
+                    }
+                    html += "</div>";
+                }
+                html += "</div>";
+            });
+            fulfilledRequestsDiv.innerHTML = html;
+        });
+    };
 
     var imgPromises = [];
-    $('#pics_carousel .img-outer-container img').each(function() {
+    $('#pics_carousel .img-outer-container img').each(function () {
         const elem = $(this);
         const img = new Image();
         const src = elem.data('src');
         const p = new Promise((resolve, reject) => {
-            img.onload = function() {
+            img.onload = function () {
                 elem.attr('src', this.src);
                 resolve();
             };
@@ -112,7 +144,7 @@ $(document).ready(function(){
     //     // orangeCircle.css('transform', `translateX(${x*.2}px) translateY(${y*.2}px) skew(25deg, -25deg)`);
     // });
 
-    $(window).on('scroll', function() {
+    $(window).on('scroll', function () {
         //Neighbors scroll animation
         $.each($('#neighbors_ticker .item'), (i, value) => {
             $(value).css('transform', `translateX(-${window.pageYOffset}px) translateY(0)`);
@@ -135,25 +167,25 @@ $(document).ready(function(){
 
     //Picture carousel
     $('#pics_carousel').owlCarousel({
-        loop:true,
-        lazyLoad:true,
-        margin:10,
-        responsiveClass:true,
+        loop: true,
+        lazyLoad: true,
+        margin: 10,
+        responsiveClass: true,
         nav: true,
         autoHeight: true,
-        navText: ["<img src='./assets/images/left.svg'>","<img src='./assets/images/right.svg'>"],
-        responsive:{
-            0:{
-                items:1,
+        navText: ["<img src='./assets/images/left.svg'>", "<img src='./assets/images/right.svg'>"],
+        responsive: {
+            0: {
+                items: 1,
             },
-            600:{
+            600: {
                 autoHeight: false,
-                items:3,
+                items: 3,
             },
-            1000:{
+            1000: {
                 autoHeight: false,
-                items:3,
-                loop:false
+                items: 3,
+                loop: false
             }
         }
     });
@@ -162,14 +194,14 @@ $(document).ready(function(){
     $('#testimony_carousel').owlCarousel({
         items: 1,
         nav: true,
-        navText: ["<img src='./assets/images/left.svg'>","<img src='./assets/images/right.svg'>"],
+        navText: ["<img src='./assets/images/left.svg'>", "<img src='./assets/images/right.svg'>"],
         loop: true,
         center: true,
         startPosition: 1,
         dots: true,
         autoHeight: false,
-        responsive:{
-            1600:{
+        responsive: {
+            1600: {
                 stagePadding: 325
             }
         }
@@ -177,7 +209,7 @@ $(document).ready(function(){
 
     //About collapse show/hide minus/plus buttons
     $('.about-content').on('show.bs.collapse', function () {
-        $('.about-content').each(function(){
+        $('.about-content').each(function () {
             $(this).collapse('hide');
         });
     });
